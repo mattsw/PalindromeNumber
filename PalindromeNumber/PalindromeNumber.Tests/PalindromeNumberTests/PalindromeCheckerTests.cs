75eff5c7cfa1b72711e7c4f6dd5f4b494a65e3d8
mattsw <mattwilhelm@cet.com>
2022-03-15 00:51:54 -0700

    Created initial tests with one failing

---
+++ PalindromeNumber/PalindromeNumber.Tests/PalindromeNumberTests/PalindromeCheckerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PalindromeNumber.Palindrome;
+
+namespace PalindromeNumber.Tests.PalindromeNumberTests
+{
+    [TestClass]
+    public class PalindromeCheckerTests
+    {
+        [TestMethod]
+        public void PalindromeChecker_ShouldIdentifyPalindromeNumber()
+        {
+            var sut = new PalindromeChecker();
+            var result = sut.IsPalindrome(121);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void PalindromeChecker_ShouldIdentifyIncorrectPalindromeNumber()
+        {
+            var sut = new PalindromeChecker();
+            var result = sut.IsPalindrome(122);
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ PalindromeNumber/PalindromeNumber/Palindrome/PalindromeChecker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeNumber.Palindrome
+{
+    public sealed class PalindromeChecker
+    {
+        public bool IsPalindrome(int numberToCheck)
+        {
+            if (numberToCheck < 0) return false;
+            return true;
+        }
+    }
+}
